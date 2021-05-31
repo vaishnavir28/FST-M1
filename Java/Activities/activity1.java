4cb3b78fba10042fd32a0a70ac0b5d9aa5e8bd46
vaishnavir28 <85126671+vaishnavir28@users.noreply.github.com>
2021-05-31 22:39:33 +0530

    Add files via upload

---
+++ Java/Activities/activity1.java
@@ -0,0 +1,33 @@
+package Activities;
+
+
+
+public class activity1 {
+
+
+
+public static void main(String[] args) {
+
+Car maruti = new Car();
+
+maruti.make = 2000;
+
+maruti.color = "Black";
+
+maruti.transmission = "Manual";
+
+
+
+//Using Car class method
+
+maruti.displayCharacterstics();
+
+maruti.accelerate();
+
+maruti.brake();
+
+}
+
+
+
+}
