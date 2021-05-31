febddd0fcb8e0e920328f25e1cc32acfff45ddff
vaishnavir28 <85126671+vaishnavir28@users.noreply.github.com>
2021-05-31 22:42:19 +0530

    Add files via upload

---
+++ Java/Activities/avtivity12.java
@@ -0,0 +1,23 @@
+package Activities;
+
+
+
+
+interface Addable {
+    int add(int a, int b);
+}
+
+public class avtivity12 {
+    public static void main(String[] args) {
+
+        // Lambda expression without return keyword.
+        Addable ad1 = (a, b) -> (a + b);
+        System.out.println(ad1.add(20, 40));
+
+        // Lambda expression with return keyword.
+        Addable ad2 = (int a, int b) -> {
+            return (a + b);
+        };
+        System.out.println(ad2.add(100, 200));
+    }
+}
